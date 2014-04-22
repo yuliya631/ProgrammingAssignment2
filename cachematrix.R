af92205fd88bd28f2d96e844bd91c4a45fe81518
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    first edits to function

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,16 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# <<- is assigning 'local' meaning to the variable in the function
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+get <- function() x
+setinverse <- function(inverse) i <<- inverse
+getinverse <- function() i
+list(set = set, 
+     get = get, 
+     setinverse = setinverse
+     getinverse = getinverse)
 }
 
 
