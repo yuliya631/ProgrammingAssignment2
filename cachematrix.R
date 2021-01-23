48eaca7001a58470f4509ccb67764b06f55104ce
yuliya631 <yuliya.palianok@gmail.com>
2021-01-23 10:35:19 -0500

    Tested and complete functions.

--- cachematrix.R
+++ cachematrix.R
@@ -2,21 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
-# <<- is assigning 'local' meaning to the variable in the function
+## <<- is assigning 'local' meaning to the variable in the function
+## Set value of a matrix
+## Get value of matrix
+## Set value of inverse
+## Get inverse
 makeCacheMatrix <- function(x = matrix()) {
-        
-get <- function() x
-setinverse <- function(inverse) i <<- inverse
-getinverse <- function() i
-list(set = set, 
-     get = get, 
-     setinverse = setinverse
-     getinverse = getinverse)
+        a <- NULL
+        set <- function(y) {
+                x <<- y
+                a <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) a <<- inverse
+        getinverse <- function() a
+        list(set = set, get = get, 
+             setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+##THe below function gets the inverse of a matrix, but
+##first checks to see if the inverse was already calculated and cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        a <-x$getinverse()
+        if(!is.null(a)) {
+                message("getting chached data")
+                return(a)
+        }
+        data <- x$get()
+        a <-solve(data, ...)
+        x$setinverse(a)
+        a
 }
